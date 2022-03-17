9b05eea0a010cb1e9e984d6b095587e34ece89fa
William Fernandes <william4fernandes@gmail.com>
2022-03-17 15:15:14 -0300

    side bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import SideBar from './components/SideBar';
 
 function App() {
   return (
-    <p>cv</p>
-  );
+
+    <SideBar />
+    );
 }
 
 export default App;
---
+++ src/components/SideBar/index.tsx
@@ -0,0 +1,16 @@
+// import { ReactComponent as Perfil } from 'assets/img/perfil.png';
+import './style.css';
+
+function SideBar() {
+
+    return (
+        <div className='barra-lateral'>
+            <p>barra lateral</p>
+
+            {/* <Perfil /> */}
+        </div>
+    );
+
+}
+
+export default SideBar;
